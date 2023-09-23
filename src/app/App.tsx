9eca13320a136e4b714e1fa08bb3f20b3c1e8308
Alexandr <vashchekin01@mail.ru>
2023-09-23 17:14:15 +0300

    feat: add modal, portal, change header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
-import React, { Suspense } from 'react';
-import './styles/index.scss';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
@@ -11,9 +10,9 @@ function App() {
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
+      <Suspense fallback=''>
         <Navbar />
-        <div className="content-page">
+        <div className='content-page'>
           <Sidebar />
           <AppRouter />
         </div>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,11 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+/* eslint-disable i18next/no-literal-string */
+import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import Modal from 'shared/ui/Modal/Modal';
+
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,9 +15,23 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
 
+  const [isAuthModal, setIsAuthModal] = useState<boolean>(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prevValue) => !prevValue);
+  }, []);
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links} />
+      <Button className={cls.links} theme={ButtonTheme.CLEAR_INVERTED} onClick={onToggleModal}>
+        {t('Войти')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam officia
+        incidunt possimus commodi et debitis, officiis quidem asperiores
+        dignissimos beatae tempora sit magnam quod sed repellat quis magni ullam
+        error?
+      </Modal>
     </div>
   );
 };
