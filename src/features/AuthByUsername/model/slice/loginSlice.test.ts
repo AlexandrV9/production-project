d64f8e081b52c6aaf608a4f1dfeed4fb36d4222d
Alexandr <vashchekin01@mail.ru>
2023-10-10 18:27:41 +0300

    fix: test bug fix

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -9,7 +9,7 @@ describe('loginSlice.test', () => {
     };
     expect(
       loginReducer(state as LoginSchema, loginActions.setUsername('123123')),
-    ).toBe({ username: '123' });
+    ).toEqual({ username: '123' });
   });
   test('test set password', () => {
     const state: DeepPartial<LoginSchema> = {
@@ -17,6 +17,6 @@ describe('loginSlice.test', () => {
     };
     expect(
       loginReducer(state as LoginSchema, loginActions.setPassword('123123')),
-    ).toBe({ password: '123' });
+    ).toEqual({ password: '123' });
   });
 });
