6ac44e4bacfe0cc1b0ed21e7d6176e412c1efc6c
Alexandr <vashchekin01@mail.ru>
2023-11-21 18:30:40 +0300

    feat: add concurrently, add templates

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -143,9 +143,10 @@ export default {
   // The glob patterns Jest uses to detect test files
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    "\\\\node_modules\\\\",
+    "\\\\.fttemplates\\\\"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
