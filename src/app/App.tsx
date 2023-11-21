d630407cd26aff107f007a004cacee6ecf53a9f2
Alexandr <vashchekin01@mail.ru>
2023-11-21 19:48:41 +0300

    refac: update protect router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,12 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 
 function App() {
   const { theme } = useTheme();
+  const inited = useSelector(getUserInited);
 
   const dispatch = useDispatch();
 
@@ -22,7 +23,7 @@ function App() {
         <Navbar />
         <div className='content-page'>
           <Sidebar />
-          <AppRouter />
+          {inited && <AppRouter />}
         </div>
       </Suspense>
     </div>
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,10 +1,12 @@
 import { userReducer, userActions } from "./model/slice/userSlice";
 import { UserSchema } from "./model/types/user";
 import { getUserAuthData } from "./model/selectors/getUserAuthData";
+import { getUserInited } from "./model/selectors/getUserInited";
 
 export {
   userReducer,
   userActions,
   UserSchema,
-  getUserAuthData
+  getUserAuthData,
+  getUserInited
 }
